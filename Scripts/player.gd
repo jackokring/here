a7591c653750036703cee12c1b8ed0a49fc009eb
jackokring <jackokring@gmail.com>
2025-02-22 06:51:24 +0000

    collider sound test

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,8 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	contact_monitor = true
+	max_contacts_reported = 16
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,3 +18,6 @@ func  _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 
 func set_frame(number: int) -> void:
 	$Sprite2D.frame = number
+
+func body_entered(body: Node) -> void:
+	$AudioStreamPlayer2D.play()
