49cf11904ef4b4e04e3582ac4104f172d2d465c7
jackokring <jackokring@gmail.com>
2025-03-04 22:21:20 +0000

    Bass test 2.

--- Scripts/audio.gd
+++ Scripts/audio.gd
@@ -7,14 +7,18 @@ var rate
 var freq = 440.0 / 8
 # zero crossing
 var phase = 0.5
+var time = 0.0
+var bpm = 120.0 * 2
+
 
 func _ready() -> void:
-	rate = stream.mix_rate
+	rate = 1 / stream.mix_rate
 	start()
 
 
 func _process(delta: float) -> void:
 	fill_buffer()
+
 
 func start():
 	play()
@@ -23,9 +27,10 @@ func start():
 
 
 func fill_buffer():
-	var inc = freq / rate
 	var frames = playback.get_frames_available()
-	
+	var inc_env = bpm / 60.0 * rate
 	for i in range(frames):
-		playback.push_frame(centre * (phase - 0.5))
+		var inc = freq * rate
+		playback.push_frame((phase - 0.5) * (1.0 - time) * centre)
 		phase = fmod(phase + inc, 1.0)
+		time = fmod(time + inc_env, 1.0)
