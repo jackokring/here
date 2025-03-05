c9a4efabc30da5d93a65a5df2215914d63530adf
jackokring <jackokring@gmail.com>
2025-03-05 05:47:57 +0000

    Bass test umm, a think about envMod.

--- Scripts/audio.gd
+++ Scripts/audio.gd
@@ -9,6 +9,7 @@ var phase: float = 0.5
 var time: float = 0.0
 var bpm: float = 120.0 * 2
 var song_pos: int = 0
+var lpf: AudioEffectFilter
 
 # Tune
 const notes: Array[float] = [
@@ -33,6 +34,7 @@ const song: Array[int] = [
 
 func _ready() -> void:
 	rate = 1 / stream.mix_rate
+	lpf = AudioServer.get_bus_effect(AudioServer.get_bus_index("Bass"), 0) as AudioEffectFilter
 	start()
 
 
@@ -48,13 +50,22 @@ func start() -> void:
 
 func fill_buffer() -> void:
 	var frames: int = playback.get_frames_available()
-	var inc_env: float = bpm / 60.0 * rate
+	var bps: float = bpm / 60.0
+	var inc_env: float = bps * rate
 	for i in range(frames):
 		var idx = (song[song_pos / pat_step % song.size()]
 		* stride + (song_pos % pat_step) * pat_para)
 		var inc: float = notes[pats[idx]] * rate
 		time = fmod(time, 1.0)
 		var env: float = 1.0 - fmod(pats[idx + 1] * time, 1.0)
+		# filter delay approximation
+		var fil_d: float = ((get_playback_position() + AudioServer.get_time_since_last_mix())
+		* bps)
+		var fil_t: float = fmod(fil_d, 1.0)
+		var fil_p: int = int(fil_d)
+		var fil_i: int = (song[fil_p / pat_step % song.size()]
+		* stride + (fil_p % pat_step) * pat_para)
+		# can set mod bus filter now
 		playback.push_frame((phase - 0.5) * env * centre)
 		phase = fmod(phase + inc, 1.0)
 		time += inc_env
