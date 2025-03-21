f2949da6f3301279c5c11ce1a0a206e8c9342e0d
jackokring <jackokring@gmail.com>
2025-03-21 20:37:24 +0000

    drum filter

--- Scripts/audio.gd
+++ Scripts/audio.gd
@@ -11,12 +11,21 @@ const bpm: float = 50.0
 const npm: float = bpm * 4.0
 var song_pos: int = 0
 var lpf: AudioEffectFilter
+var dlpf: AudioEffectFilter
 var last_drum: int = -1
 # Tune
 const notes: PackedFloat32Array = [
 	27.5, 29.13524, 30.86771, 32.7032,
 	34.64783, 36.7081, 38.89087, 41.20344,
 	43.65353, 46.2493, 48.99943, 51.91309,
+	
+	27.5 * 2, 29.13524 * 2, 30.86771 * 2, 32.7032 * 2,
+	34.64783 * 2, 36.7081 * 2, 38.89087 * 2, 41.20344 * 2,
+	43.65353 * 2, 46.2493 * 2, 48.99943 * 2, 51.91309 * 2,
+	
+	27.5 * 4, 29.13524 * 4, 30.86771 * 4, 32.7032 * 4,
+	34.64783 * 4, 36.7081 * 4, 38.89087 * 4, 41.20344 * 4,
+	43.65353 * 4, 46.2493 * 4, 48.99943 * 4, 51.91309 * 4,
 ]
 var drum: Array[AudioStreamPlayer]
 const env_mod: PackedFloat32Array = [
@@ -26,14 +35,14 @@ const env_mod: PackedFloat32Array = [
 	16.0, 3.0, 3.0,
 ]
 const pat_step = 4
-const pat_para = 4
+const pat_para = 6
 const stride = pat_para * pat_step
 const pats: PackedByteArray = [
-	#0 [Note], stutterCount, [envMod], [drum]
-	0, 2, 0, 0,
-	9, 1, 1, 4,
-	5, 4, 0, 1,
-	7, 1, 0, 2,
+	#0 [Note], stutterCount, [envMod], [drum], [drumFiltNote], [drumEnvMod]
+	0, 2, 0, 0, 0, 0,
+	9, 1, 1, 4, 9, 1,
+	5, 4, 0, 1, 5, 0,
+	7, 1, 0, 2, 7, 1,
 ]
 # 256 pattern limit
 const song: PackedByteArray = [
@@ -44,6 +53,7 @@ const song: PackedByteArray = [
 func _ready() -> void:
 	rate = 1 / stream.mix_rate
 	lpf = AudioServer.get_bus_effect(AudioServer.get_bus_index("Bass"), 0) as AudioEffectFilter
+	dlpf = AudioServer.get_bus_effect(AudioServer.get_bus_index("Drum"), 0) as AudioEffectFilter
 	drum = [
 		#0 to 4
 		$Kick, $Snare, $Clap, $Open, $Closed
@@ -74,12 +84,19 @@ func fill_buffer() -> void:
 	* stride + (fil_p % pat_step) * pat_para)
 	# can set mod bus filter now
 	var mod: int = pats[fil_i + 2]
+	var dmod: int = pats[fil_i + 5]
 	var freq: float = notes[pats[fil_i]]
 	var df: float = env_mod[3 * mod + 1] * fil_t
 	freq *= env_mod[3 * mod] * (1.0 + df)
 	var rez: float = env_mod[3 * mod + 2]
 	lpf.cutoff_hz = freq
 	lpf.resonance = rez
+	var dfreq: float = notes[pats[fil_i + 4]]
+	var ddf: float = env_mod[3 * dmod + 1] * fil_t
+	dfreq *= env_mod[3 * dmod] * (1.0 + ddf)
+	var drez: float = env_mod[3 * dmod + 2]
+	dlpf.cutoff_hz = dfreq
+	dlpf.resonance = drez
 	# and some drum
 	if fil_i != last_drum:
 		var d: AudioStreamPlayer = drum[pats[fil_i + 3]]
