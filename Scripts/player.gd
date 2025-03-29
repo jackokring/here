4ade0e721537aeb718b3479b9b959811602c4690
jackokring <jackokring@gmail.com>
2025-03-29 05:25:23 +0000

    Get Loaded

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,22 @@
 extends RigidBody2D
+var loaded: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body_shape_entered.connect(on_body_shape_entered)
 	contact_monitor = true
 	max_contacts_reported = 16
+	loader()
+
+func loader() -> void:
+	loaded = false
+	modulate = 0xffffff7f
+	$Loading.start()
 	position = Global.get_random_position(Global.rand)
+
+func end_loader() -> void:
+	modulate = 0xffffffff
+	loaded = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,6 +31,7 @@ func  _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 
 # atlas pos collide 
 func map_collide(atlas: Array[Vector2i], id: Vector2i) -> void:
+	# maybe not "loaded" delay for collides
 	match atlas:
 		Atlas.field:
 			match id:
@@ -29,6 +41,7 @@ func map_collide(atlas: Array[Vector2i], id: Vector2i) -> void:
 # body collide
 # use body for information exchange?
 func body_collide(kind: int, body: RigidBody2D):
+	# maybe not "loaded" delay for collides
 	match kind:
 		Body.PLAYER:
 			pass
